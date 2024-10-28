ddae8eac0b44068a7e92fec339ec55a207599c82
L-PDufour <leonpierre.dufour@gmail.com>
2024-10-28 08:42:12 -0400

    miriokucou

--- keyboards/crkbd/keymaps/l-pdufour/keymap.c
+++ keyboards/crkbd/keymaps/l-pdufour/keymap.c
@@ -1,21 +1,3 @@
-/*
-- **Bases de données** : SQL
-Copyright 2019 @foostan
-Copyright 2020 Drashna Jaelre <@drashna>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #include QMK_KEYBOARD_H
 
@@ -23,6 +5,7 @@ enum layer_names {
     _BASE,
     _NAV,
     _NUM,
+    _FUN,
 };
 
 enum custom_keycodes {
@@ -32,20 +15,23 @@ enum custom_keycodes {
     CKC_D,
     CKC_F,
 
-    CKC_TAB,
     CKC_SPC,
 
     CKC_J,
     CKC_K,
     CKC_L,
     CKC_SCLN,
+    CKC_DOT,
+
+    CKC_BSPC,
+    CKC_DEL,
 
     SMTD_KEYCODES_END,
 };
 
 #include "sm_td.h"
 // https://github.com/stasmarkin/sm_td
-// https://github.com/azzamsa/qmk_userspace/blob/main/keyboards/sofle/keymaps/azzamsa/keymap.c
+// Https://github.com/azzamsa/qmk_userspace/blob/main/keyboards/sofle/keymaps/azzamsa/keymap.c
 // Layer definitions
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -55,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_RBRC,   CKC_A,   CKC_S,   CKC_D,   CKC_F,    KC_G,                         KC_H,   CKC_J,   CKC_K,   CKC_L,CKC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_GRV,
+      KC_CAPS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  CKC_DOT, KC_SLSH, KC_GRV,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_ESC, CKC_SPC, CKC_TAB,    KC_BSPC,  KC_ENT, KC_RALT
+                                           KC_ESC, CKC_SPC, KC_TAB,    KC_ENT,  CKC_BSPC, CKC_DEL
                                       //`--------------------------'  `--------------------------'
     ),
 
@@ -70,18 +56,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                       KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_MUTE,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                            KC_NO ,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO
-                                      //`--------------------------'  `--------------------------'
+                                    //`--------------------------'  `--------------------------'
     ),
 
     [_NUM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                         KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,
+        KC_NO, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT,   KC_NO,                        KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_RALT,   KC_NO,
+        KC_NO, KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL,                        KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,   KC_NO,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
+        KC_NO,   KC_GRV,   KC_1,    KC_2,    KC_3, KC_BSLS,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                           KC_NO ,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO
+                                           KC_DOT,   KC_0,  KC_MINS,      KC_NO,   KC_NO,  KC_RALT
+                                      //`--------------------------'  `--------------------------'
+    ),
+    [_FUN] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                        KC_NO, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,   KC_NO,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                           KC_NO ,   KC_NO,   KC_NO,      KC_NO,   KC_NO,  KC_RALT
                                       //`--------------------------'  `--------------------------'
     ),
  };
@@ -95,68 +92,19 @@ void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
         SMTD_MT(CKC_F, KC_F, KC_LSFT)   // F with Left Shift when held
 
         // Layer Tap Definitions
-        SMTD_LT(CKC_SPC, KC_SPACE, _NUM) // Space bar for number layer
-        SMTD_LT(CKC_TAB, KC_TAB, _NAV)    // Tab for navigation layer
+        SMTD_LT(CKC_SPC, KC_SPACE, _NAV) // Space bar for number layer
 
         // Additional Modifier Taps
         SMTD_MT(CKC_J, KC_J, KC_LSFT)    // J with Left Shift when held
         SMTD_MT(CKC_K, KC_K, KC_LCTL)    // K with Left Control when held
         SMTD_MT(CKC_L, KC_L, KC_LALT)    // L with Left ALT when held
         SMTD_MT(CKC_SCLN, KC_SCLN, KC_LGUI) // Semicolon with Left GUI when held
+        SMTD_MT(CKC_DOT, KC_DOT, KC_RALT) // Semicolon with Left GUI when held
+
+        SMTD_LT(CKC_DEL, KC_DEL, _FUN) // Space bar for number layer
+        SMTD_LT(CKC_BSPC, KC_BSPC, _NUM) // Space bar for number layer
 }
 }
-
-uint32_t get_smtd_timeout(uint16_t keycode, smtd_timeout timeout) {
-    switch (keycode) {
-        // left side
-        case CKC_A:
-            if (timeout == SMTD_TIMEOUT_TAP) return 350;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-        case CKC_S:
-            if (timeout == SMTD_TIMEOUT_TAP) return 300;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-        case CKC_D:
-            if (timeout == SMTD_TIMEOUT_TAP) return 300;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-        case CKC_F:
-            if (timeout == SMTD_TIMEOUT_TAP) return 300;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-
-        case CKC_SPC:
-            if (timeout == SMTD_TIMEOUT_TAP) return 300;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-        case CKC_TAB:
-            if (timeout == SMTD_TIMEOUT_TAP) return 300;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-
-        // right side
-        case CKC_J:
-            if (timeout == SMTD_TIMEOUT_TAP) return 300;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-        case CKC_K:
-            if (timeout == SMTD_TIMEOUT_TAP) return 300;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-        case CKC_L:
-            if (timeout == SMTD_TIMEOUT_TAP) return 300;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-        case CKC_SCLN:
-            if (timeout == SMTD_TIMEOUT_TAP) return 350;
-            if (timeout == SMTD_TIMEOUT_RELEASE) return 20;
-            break;
-   }
-
-    return get_smtd_timeout_default(timeout);
-}
-
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_smtd(keycode, record)) {
--- keyboards/crkbd/keymaps/l-pdufour/rules.mk
+++ keyboards/crkbd/keymaps/l-pdufour/rules.mk
@@ -1,20 +1,7 @@
 # Feature enable/disable flags
 
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes      # Audio control and System control
-CONSOLE_ENABLE = no       # Console for debug
-COMMAND_ENABLE = no        # Commands for debug and configuration
-NKRO_ENABLE = yes          # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
-MIDI_ENABLE = no           # MIDI support
-AUDIO_ENABLE = no          # Audio output
-UNICODE_ENABLE = no        # Unicode
-BLUETOOTH_ENABLE = no      # Enable Bluetooth
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight
-RGB_MATRIX_ENABLE = no     # Matrix LEDs
-LTO_ENABLE = yes
-KEY_LOCK_ENABLE = no
+OLED_ENABLE = no
+RGBLIGHT_ENABLE = no
+
+# sm_td
 DEFERRED_EXEC_ENABLE = yes
-
-CFLAGS += -flto
-
