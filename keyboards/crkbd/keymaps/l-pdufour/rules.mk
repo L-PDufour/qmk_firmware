526af06de34e0e2fa548ffa38a15d68d2aab3df2
L-PDufour <leonpierre.dufour@gmail.com>
2024-11-16 21:39:04 -0500

    refactor

--- keyboards/crkbd/keymaps/l-pdufour/rules.mk
+++ keyboards/crkbd/keymaps/l-pdufour/rules.mk
@@ -1,8 +1,9 @@
 # Feature enable/disable flags
-SRC += oneshot.c
+# SRC += oneshot.c
 OLED_ENABLE = no
 RGBLIGHT_ENABLE = no
+CAPS_WORD_ENABLE = yes
 # COMBO_ENABLE = yes
 
 # sm_td
-DEFERRED_EXEC_ENABLE = yes
+# DEFERRED_EXEC_ENABLE = yes
