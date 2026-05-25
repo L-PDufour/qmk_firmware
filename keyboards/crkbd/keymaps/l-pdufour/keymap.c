e35cf611c9fd63b6c3755bc8cd177a9afa56c948
L-PDufour <leonpierre.dufour@gmail.com>
2026-05-25 10:41:53 -0400

    fix layers

--- keyboards/crkbd/keymaps/l-pdufour/keymap.c
+++ keyboards/crkbd/keymaps/l-pdufour/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 enum layer_names {
     _BASE,
+    _SYM,
     _NAV,
     _NUM,
     _FUN,
@@ -44,47 +45,49 @@ combo_t key_combos[] = {
 #define WM_5 LGUI(KC_5)
 
 #define CKC_SPC LT(_NAV, KC_SPC)
+
+#define CKC_ENT LT(_SYM, KC_ENT)
 #define CKC_BSPC LT(_NUM, KC_BSPC)
-#define CKC_ESC LT(_FUN, KC_ESC)
+#define CKC_DEL LT(_FUN, KC_DEL)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
-          KC_NO,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_NO,
-          KC_NO,    HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                         KC_H,    HM_J,    HM_K,    HM_L, HM_SCLN,   KC_NO,
-          KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO,
-                                             CKC_ESC, CKC_SPC, KC_TAB,    KC_ENT, CKC_BSPC,  KC_DEL
+          KC_NO,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_NO,
+          KC_NO,    HM_A,    HM_S,    HM_D,    HM_F,    KC_G,    KC_H,    HM_J,    HM_K,    HM_L, HM_SCLN,   KC_NO,
+          KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO,
+                                     KC_ESC, CKC_SPC, KC_TAB,    CKC_ENT, CKC_BSPC,  CKC_DEL
       ),
-    [_NAV] = LAYOUT_split_3x6_3(
-          KC_NO,   KC_NO, KC_VOLD, KC_MUTE, KC_VOLU, KC_COPY,                        KC_NO, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT,   KC_NO,
-          KC_NO, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_PSTE,                      KC_CAPS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   KC_NO,
-          KC_NO,    WM_1,    WM_2,    WM_3,    WM_4,    WM_5,                       KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   KC_NO,
-                                             KC_NO,  XXXXXXX,   KC_NO,      KC_ENT, KC_BSPC,  KC_DEL
+    [_SYM] = LAYOUT_split_3x6_3(
+          KC_NO, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+          KC_NO, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,    KC_NO, OS_LCTL, OS_LSFT, OS_LALT, OS_LGUI, KC_NO,
+          KC_NO, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,    KC_NO, KC_NO,   KC_NO,   OS_RALT, KC_NO,   KC_NO,
+                                   KC_LPRN, KC_RPRN, KC_UNDS,    XXXXXXX, KC_NO, KC_NO
       ),
-[_NUM] = LAYOUT_split_3x6_3(
-  KC_NO, KC_NO,   KC_7, KC_8, KC_9, KC_NO,      KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-  KC_NO, KC_SLSH, KC_4, KC_5, KC_6, KC_ASTR,    KC_NO, OS_LGUI, OS_LALT, OS_LSFT, OS_LCTL, KC_NO,
-  KC_NO, KC_MINS, KC_1, KC_2, KC_3, KC_PLUS,    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-                        KC_DOT, KC_0, KC_EQL,   KC_NO, XXXXXXX, KC_NO
-                            ),
-[_SYM] = LAYOUT_split_3x6_3(
-  KC_NO, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO,
-  KC_NO, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_PIPE,    KC_BSLS, OS_LGUI, OS_LALT, OS_LSFT, OS_LCTL, KC_NO,
-  KC_NO, KC_GRV,  KC_TILD, KC_MINS, KC_EQL,  KC_PLUS,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                            KC_NO, XXXXXXX,   KC_NO,      KC_NO, XXXXXXX, KC_NO
-),
+    [_NUM] = LAYOUT_split_3x6_3(
+                   KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                   KC_NO, KC_SCLN, KC_4, KC_5, KC_6,  KC_EQL,    KC_NO, OS_LCTL, OS_LSFT, OS_LALT, OS_LGUI, KC_NO,
+                   KC_NO, KC_GRV,  KC_1, KC_2, KC_3, KC_BSLS,    KC_NO, KC_NO,   KC_NO,   OS_RALT, KC_NO,   KC_NO,
+                                       KC_DOT, KC_0, KC_MINS,    KC_NO, XXXXXXX, KC_NO
+      ),
    [_FUN] = LAYOUT_split_3x6_3(
-           KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_NO,
-          KC_NO, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL,   KC_NO,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_NO,
-          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_F11,  KC_F12,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                           XXXXXXX,    KC_NO,   KC_NO,   TO(_GAMING), OSM(MOD_RALT), CW_TOGG
+                 KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,    KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL,    KC_NO, OS_LCTL, OS_LSFT, OS_LALT, OS_LGUI, KC_NO,
+                 KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS,    KC_NO, KC_NO,   KC_NO,   OS_RALT, KC_NO,   KC_NO,
+                               TO(_GAMING), CW_TOGG, KC_CAPS,    KC_NO, KC_NO, XXXXXXX
       ),
-    [_GAMING] = LAYOUT_split_3x6_3(
+   [_NAV] = LAYOUT_split_3x6_3(
+             KC_NO, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_NO,    KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, KC_VOLU, KC_NO,
+             KC_NO, OS_LGUI,  OS_LALT, OS_LSFT, OS_LCTL, KC_NO,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MUTE, KC_NO,
+             KC_NO, WM_1,    WM_2,   WM_3,    WM_4,     WM_5,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_VOLD, KC_NO,
+                                       KC_NO, XXXXXXX, KC_NO,    KC_NO, KC_NO, KC_NO
+      ),
+   [_GAMING] = LAYOUT_split_3x6_3(
           KC_NO,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_NO,
           KC_NO,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,   KC_NO,
           KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,TO(_BASE),   KC_NO,
-                                            CKC_ESC, CKC_SPC, KC_TAB,    KC_ENT, MO(_NUM),  MO(_FUN)
-      ),
+                                            KC_ESC, KC_SPC, KC_TAB,    KC_ENT, MO(_NUM),  MO(_FUN)
+                                   ),
  };
 
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
